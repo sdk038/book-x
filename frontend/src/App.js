4d4d4a99b07c1f7c8da454eed68d7f2462bcd69f
dalersabirov <sobrovv08@gmail.com>
2026-02-17 17:56:33 +0500

    task-3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BooksProvider } from './context/BooksContext';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { Favorites } from './pages/Favorites';
+import { Chats } from './pages/Chats';
+import { NewListing } from './pages/NewListing';
+import { BookDetails } from './pages/BookDetails';
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <BooksProvider>
+            <Router>
+                <div className="app">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/favorites" element={<Favorites />} />
+                        <Route path="/chats" element={<Chats />} />
+                        <Route path="/new" element={<NewListing />} />
+                        <Route path="/book/:id" element={<BookDetails />} />
+                    </Routes>
+                </div>
+            </Router>
+        </BooksProvider>
     );
 };
 
 export default App;
+
